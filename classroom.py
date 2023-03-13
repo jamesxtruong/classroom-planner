3398c4a88b9cda68a54bb79b970c3731065b38ad
James Truong <jatruong@pdx.edu>
2023-03-12 22:18:30 -0700

    Update 3-12-23

--- classroom.py
+++ classroom.py
@@ -3,22 +3,46 @@
 #James Truong   CS302   3-12-2023
 #Program 4/5    Classroom Planner Program 
 
-#This is the file that contains the class interface(s).
-#There is a base class called classroom and it's subclasses are
-#different types of classes such as: computer lab, lecture, and remote.
+"""
+This is the file that contains the class interface(s).
+There is a base class called classroom and it's subclasses are
+different types of classes such as: computer lab, lecture, and remote.
+"""
+class Classroom:                #Base class 
+    def __init__(self, occupants = 0, capacity = 0, special_notes = "", technology = "",
+                 ada = True):    #Default constructor
+        self._occupants = occupants         #Number of occupants in class
+        self._capacity = capacity           #Capacity of classroom for occupants
+        self._special_notes = special_notes #Special notes for classroom
+        self._technology = technology       #Technology info for classroom
+        self._ada = ada                     #ADA accessible classroom 
 
-class Classroom:
-    def __init__(self)          #Default constructor
-        self._capacity = 0
-        self._occupants = 0
-        self._special_notes = ""
-        self._technology_capabilities = ""
-        self._ada = True 
-    def print_info(self)        #Print information about classroom
+    def print_info(self):        #Print information about classroom
+        print("\nClassroom info: \n")
+        print(f"Occupants: {self._occupants} | Capacity: {self._capacity}")
+        #print(f"Capacity: {self._capacity}")
+        print(f"Special Notes: {self._special_notes}")
+        print(f"Technology info: {self._technology}")
+        if(self._ada):
+            print("ADA Accessible: Yes")
+        else:
+            print("ADA Accessible: No")
+        return None
+        #pass 
+    def change_occupants(self, occupants = None):
+        pass 
+    def change_capacity(self, capacity = None):
+        pass
+    def change_notes(self, special_notes = None): 
+        pass
+    def change_tech(self, technology = None): 
+        pass
+    def change_ada(self, ada = None):
+        pass 
 
-class Computer_lab(Classroom):
+class Computer_lab(Classroom):  #Computer lab sub-class, is a Classroom 
     pass
-class Lecture(Classroom):
+class Lecture(Classroom):       #Lecture is a sub-class, is a Classroom
     pass
-class Remote(Classroom):
+class Remote(Classroom):        #Remote is a sub-class, is a Classroom 
     pass 
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 #main.py
+from classroom import Classroom 
 
 #James Truong   CS302	    3-12-2023
 #Program 4/5	Classroom Planner Program
 
 """ 
-This is the client test application. 
+This is the client test application. The software will be tested
+through a series of calls from a menu interface. The application will
+allow for schedule and planning of classrooms. The different types of 
+classrooms are computer lab, lecture, and remote. 
 """
 
 def main():
+    print("\nWelcome to the Classroom Planner Program!\n")
 
-if __name_ == "__main__":
+    a_class = Classroom(1, 5, "The Base Class", "Basic Tech", True)
+    b_class = Classroom()
+    #a_class = Classroom(1, 5, "The Base Class", "Basic Tech", False)
+    #print(a_class.print_info()) #returns None at end 
+    a_class.print_info()
+    b_class.print_info()
+
+
+if __name__ == "__main__":
     main()
